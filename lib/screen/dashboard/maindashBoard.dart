5c0802de53ff25df25ed302ba1f0f62319305b09
unknown <vegaKannur12>
2022-09-28 13:59:44 +0530

    nsdb

--- lib/screen/dashboard/maindashBoard.dart
+++ lib/screen/dashboard/maindashBoard.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:gulferp/components/commonColor.dart';
+import 'package:gulferp/screen/loginPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainDashboard extends StatefulWidget {
   const MainDashboard({Key? key}) : super(key: key);
@@ -13,16 +16,80 @@ class MainDashboard extends StatefulWidget {
 class _MainDashboardState extends State<MainDashboard> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return WillPopScope(
       onWillPop: () => _onBackPressed(context),
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: P_Settings.loginPagetheme,
+          automaticallyImplyLeading: false,
+          actions: [
+            PopupMenuButton(
+                // add icon, by default "3 dot" icon
+                // icon: Icon(Icons.book)
+                itemBuilder: (context) {
+              return [
+                PopupMenuItem<int>(
+                  value: 0,
+                  child: Text("Refresh"),
+                ),
+                PopupMenuItem<int>(
+                  value: 1,
+                  child: Text("Logout"),
+                ),
+              ];
+            }, onSelected: (value) async {
+              if (value == 0) {
+                // Provider.of<Controller>(context, listen: false).userDetails();
+                // Provider.of<Controller>(context, listen: false)
+                // .getStockApprovalList(context);
+              } else if (value == 1) {
+                final prefs = await SharedPreferences.getInstance();
+                await prefs.remove('st_username');
+                await prefs.remove('st_pwd');
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => LoginPage()));
+                print('Pressed');
+              }
+            }),
+          ],
         ),
         body: Container(
           height: double.infinity,
           child: Column(
-            children: [Text("DashBoard")],
+            children: [
+              Container(
+                height: size.height * 0.1,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Image.asset("asset/login.png"),
+                  ),
+                  title: Text(
+                    "anu",
+                    // value.staff_name.toString(),
+                    style: GoogleFonts.aBeeZee(
+                      textStyle: Theme.of(context).textTheme.bodyText2,
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: P_Settings.buttonColor,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "KNR",
+                    // value.branch_name.toString(),
+                    style: GoogleFonts.aBeeZee(
+                      textStyle: Theme.of(context).textTheme.bodyText2,
+                      fontSize: 14,
+                      // fontWeight: FontWeight.bold,
+                      color: P_Settings.buttonColor,
+                    ),
+                  ),
+                  dense: false,
+                ),
+                color: P_Settings.loginPagetheme,
+              ),
+            ],
           ),
         ),
       ),
