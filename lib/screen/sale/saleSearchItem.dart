7e285c90b384f8dfeb4e5b8577039adade0dada9
vegaKannur12 <100991705+vegaKannur12@users.noreply.github.com>
2022-10-19 10:53:42 +0530

    search button

--- lib/screen/sale/saleSearchItem.dart
+++ lib/screen/sale/saleSearchItem.dart
@@ -132,12 +132,12 @@ class _SaleSearchItemState extends State<SaleSearchItem> {
       appBar: AppBar(
         backgroundColor: P_Settings.loginPagetheme,
       ),
-      body: SingleChildScrollView(child: Consumer<Controller>(
+      body: Consumer<Controller>(
         builder: (context, value, child) {
           return Column(
             children: [
               Padding(
-                padding: const EdgeInsets.only(left: 10.0, right: 10,top: 10),
+                padding: const EdgeInsets.only(left: 10.0, right: 10, top: 10),
                 child: Card(
                   elevation: 12,
                   child: ListTile(
@@ -169,8 +169,8 @@ class _SaleSearchItemState extends State<SaleSearchItem> {
               SizedBox(
                 height: size.height * 0.03,
               ),
-              Container(
-                height: size.height * 0.9,
+              Expanded(
+                // height: size.height * 0.7,
                 child: value.isLoading
                     ? SpinKitFadingCircle(
                         color: P_Settings.loginPagetheme,
@@ -727,7 +727,7 @@ class _SaleSearchItemState extends State<SaleSearchItem> {
             ],
           );
         },
-      )),
+      ),
     );
   }
 }
